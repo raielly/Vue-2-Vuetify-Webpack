46be9117ac763157f6e89b6ca0ec20115cb4821e
raielly <raielamoroto@gmail.com>
2023-03-08 23:40:45 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,28 @@ module.exports = {
         test: /\.vue$/,
         loader: "vue-loader",
       },
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            // Requires sass-loader@^7.0.0
+            options: {
+              implementation: require("sass"),
+              indentedSyntax: true, // optional
+            },
+            // Requires >= sass-loader@^8.0.0
+            options: {
+              implementation: require("sass"),
+              sassOptions: {
+                indentedSyntax: true, // optional
+              },
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
